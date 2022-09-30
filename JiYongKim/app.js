2a0d13ec2a68677fc8c23c45d11575abacddabbd
JiYongKim <colorful8315@gmail.com>
2022-10-01 02:55:07 +0900

    feat (matchingApplyRoute) : Add applyPage

--- JiYongKim/app.js
+++ JiYongKim/app.js
@@ -53,13 +53,14 @@ const loginPageRoute = require('./routes/loginPageRoute');
 const logoutPageRoute = require('./routes/logoutPageRoute');
 const signUpPageRoute = require('./routes/signUpPageRoute');
 const myPageRoute = require('./routes/myPageRoute');
+const matchingApplyRoute = require('./routes/matchingApplyPageRoute');
 
 app.use('/',mainPageRoute);
 app.use('/login',loginPageRoute);
 app.use('/logout',logoutPageRoute);
 app.use('/signup',signUpPageRoute);
 app.use('/mypage', myPageRoute);
-
+app.use('/apply',matchingApplyRoute);
 
 server.listen(process.env.PORT, () => console.log(process.env.PORT+`Port Server is open!!`))
 
